a89d76f8e591cff11e949c0a228942628611de7e
fedegy <fede88662@gmail.com>
2022-07-02 23:56:30 -0600

    Se agrego clasificador gausiano

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from pyexpat import features
+from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import PolynomialFeatures
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -45,7 +47,7 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
 
     if extension_archivo == 'CSV':
-        dataframe = pd.read_csv(uploaded_file)
+        dataframe = pd.read_csv(uploaded_file, header=None)
         st.dataframe(dataframe)
         flag = True
     elif extension_archivo == 'XLS':
@@ -62,8 +64,8 @@ if uploaded_file is not None:
         flag = True
 
 def regresion_lineal():
-    columna = st.selectbox('Seleccione X', (dataframe.columns))
-    columna2 = st.selectbox('Seleccione Y', (dataframe.columns))
+    columna = st.selectbox('Seleccione X', (dataframe.columns), index=0)
+    columna2 = st.selectbox('Seleccione Y', (dataframe.columns), index=1)
     X = np.asarray(dataframe[columna]).reshape(-1, 1)
     Y = dataframe[columna2]
 
@@ -90,8 +92,8 @@ def regresion_lineal():
 
 def regresion_polinomial():
     dtf = dataframe.columns.to_list()
-    index1 = st.selectbox("Seleccione X", range(len(dtf)), format_func=lambda x: dtf[x])
-    index2 = st.selectbox("Seleccione Y", range(len(dtf)), format_func=lambda x: dtf[x])
+    index1 = st.selectbox("Seleccione X", range(len(dtf)), format_func=lambda x: dtf[x], index=0)
+    index2 = st.selectbox("Seleccione Y", range(len(dtf)), format_func=lambda x: dtf[x], index=1)
 
     X = dataframe.iloc[:, int(index1)].values.reshape(-1, 1)
     Y = dataframe.iloc[:, int(index2)].values.reshape(-1, 1)
@@ -118,6 +120,33 @@ def regresion_polinomial():
     Y_new = linear_regression.predict(poly.fit_transform([[val_prediccion]]))
     st.write("Predicción: ", Y_new)
 
+def clasificador_gauseano():
+    st.write("# Clasificador gauseano")
+    dataframe.columns = range(dataframe.shape[1])
+    st.write(dataframe)
+    X = np.array(dataframe)
+
+    index = X.shape[1]
+    X = np.delete(X, int(index) - 1, axis=1)
+    play_column = np.array(dataframe)
+    play_column = play_column.T[int(index) - 1]
+
+    lista_play = []
+    for i in play_column:
+        lista_play.append(i)
+    lt = X.T.tolist()
+    features = list(zip(*lt))
+    model = GaussianNB()
+    model.fit(features, lista_play)
+    val = st.text_input('Ingrese entrada: ')
+    if val == "":
+        val = ""
+    else:
+        cadena = val.split(',')
+        entrada = [int(x) for x in cadena]
+        predicted = model.predict([entrada])
+        st.write("Predicción: ", predicted)
+
 
 selected = option_menu(
     menu_title="Menú",
@@ -136,7 +165,7 @@ if flag:
         st.title(f'Regresión Polinomial')
         regresion_polinomial()
     if selected == "Clasificador Gausiano":
-        st.title(f'Clasificador Gausiano')
+        clasificador_gauseano()
         st.write("Clasificador Gausiano")
     if selected == "Clasificador de arboles de desicion":
         st.title(f'Clasificador de arboles de desicion')
