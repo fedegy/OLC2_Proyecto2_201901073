70a1fcc7b49d940e66ffb29d7f306f630cc129f0
fedegy <fede88662@gmail.com>
2022-07-05 16:05:48 -0600

    Cambios finales

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-primaryColor = '#E694FF'
-backgroundColor = '#00172B'
-secondaryBackgroundColor = '#0083B8'
-textColor = '#C6CDD4'
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit/config.toml*/
+" > ~/.streamlit/config.toml
